caeaeae719007b40f118a70efce96bd561794c86
MatheusRoichman <roichmankill@gmail.com>
2021-12-11 10:22:08 -0300

    FEAT: Sliders adicionados com imagens de divulgação com os alunos.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 const toggleSidebar = () => {
   document.querySelector('#mobile-navbar').classList.toggle('active');
 };
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+
+const alunos_swiper = document.querySelector('#alunos-swiper');
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 9) i += 2;
+  
+  const slide = document.createElement('div')
+  slide.className = 'swiper-slide';
+  
+  const slide_img = document.createElement('img');
+  slide_img.src = `/img/alunos/alunos-${i}.jpeg`;
+  slide_img.alt = 'Alunos do Curso Técnico em Informática na escola.';
+  
+  slide.append(slide_img);
+  
+  alunos_swiper.append(slide);
+}
