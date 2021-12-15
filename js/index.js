3f24e4393338e35881cd0b2384efd57c0cd5074f
MatheusRoichman <roichmankill@gmail.com>
2021-12-15 13:21:35 -0300

    Melhoria no tamanho das imagens e matéria Noções de Robótica adicionada

--- js/index.js
+++ js/index.js
@@ -16,15 +16,14 @@ const swiper = new Swiper('.swiper', {
 
 const alunos_swiper = document.querySelector('#alunos-swiper');
 
-for (let i = 1; i <= 20; i++) {
-  if (i == 9) i += 2;
-  
+for (let i = 1; i <= 18; i++) {
   const slide = document.createElement('div')
   slide.className = 'swiper-slide';
   
   const slide_img = document.createElement('img');
   slide_img.src = `/img/alunos/alunos-${i}.jpeg`;
   slide_img.alt = 'Alunos do Curso Técnico em Informática na escola.';
+  slide_img.loading = 'lazy';
   
   slide.append(slide_img);
   
